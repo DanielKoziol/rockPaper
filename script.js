286fb9f32ce306e0c7451049dd4ef5fcf58f54a0
DanielKoziol <koziol.daniel@interia.pl>
2021-12-17 00:54:08 +0100

    finished first version

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 
 function RandomNumber(){
     let randomN = Math.random();
-    console.log(randomN);
+    console.log("randomN" + randomN);
     console.log(Math.floor(randomN * 3))
     return Math.floor(randomN * 3)
     };
 
-let random =  RandomNumber();
 
 const gameOptions = ["rock", "paper", "scissors"];
 
 function computerSelection (){
-    return gameOptions[random];
+    return gameOptions[RandomNumber()];
     }
-console.log (`computer: ${computerSelection()}`);
-const computerSelect = computerSelection();
 
-const promptOption = prompt("rock paper scissors, man!", "rock");
-const mySelect = promptOption.toLowerCase();
-console.log(`player: ${mySelect}`);
+    
+function playRound () {
 
-function playRound (human, computer) {
+    let promptOption = prompt("rock paper scissors, man!", "rock");
+    let mySelect = promptOption.toLowerCase();
+    let computerSelect = computerSelection();
+    console.log("computer:" + computerSelect);
 
     if (mySelect==="rock" && computerSelect==="rock") {
     result = "Draw";
@@ -28,15 +27,57 @@ function playRound (human, computer) {
     else if (mySelect==="rock" && computerSelect==="paper") {
     result = "Computer"; 
 }
-    else if (mySelect==="rock" && computerSelect==="scissors")
+    else if (mySelect==="rock" && computerSelect==="scissors") {
     result = "Player";
+    }
+    else if (mySelect==="paper" && computerSelect==="paper") {
+        result = "Draw"; 
+    }
+    else if (mySelect==="paper" && computerSelect==="scissors") {
+        result = "Computer"; 
+    }
+    else if (mySelect==="paper" && computerSelect==="rock") {
+        result = "Player";
+        }    
+    else if (mySelect==="scissors" && computerSelect==="scissors") {
+        result = "Draw"; 
+    }
+    else if (mySelect==="scissors" && computerSelect==="rock") {
+        result = "Computer"; 
+    }
+    else if (mySelect==="scissors" && computerSelect==="paper") {
+        result = "Player";
+        }    
 
     else 
     result = "I know you're cheating!";
 
 console.log(`${result} wins`);
 return result;
-
 }
 
-playRound (mySelect, computerSelect);
+let playerResult = 0;
+let computerResult = 0;
+
+function game () {
+for (let index = 0; index < 6; index++) {
+    let roundResult = playRound ();   
+    if (roundResult == "Player") {
+        playerResult++;
+        console.log("playerResult:" + playerResult);
+    }
+    else if (roundResult == "Computer") {
+        computerResult++;
+        console.log("compResult:" + computerResult);
+    }
+    else if (roundResult == "Draw") {
+        continue;
+    }
+
+}
+};
+
+game ();
+
+console.log("finalCompResult:" + computerResult);
+console.log("finalPlayerResult:" + playerResult);
